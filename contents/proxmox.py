5d37c720fddbff6f5d65bc8edac6de489c1b3847
saulotarsobc <saulotarsobc@gmail.com>
2021-07-09 20:47:24 -0300

    C

--- contents/proxmox.py
+++ contents/proxmox.py
@@ -1,3 +1,3 @@
 from proxmoxer import ProxmoxAPI
-proxmox = ProxmoxAPI('172.33.255.2', user='root@pam', password='10l15p130A@', verify_ssl=False)
+proxmox = ProxmoxAPI('SERVER_IP', user='root@pam', password='SENHA', verify_ssl=False)
 print(proxmox.cluster.resources.get())
