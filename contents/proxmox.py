5fcfbc7d88016c35fff371423f9be524fab4fe77
saulotarsobc <saulotarsobc@gmail.com>
2021-07-13 14:14:06 -0300

    Update proxmox.py

--- contents/proxmox.py
+++ contents/proxmox.py
@@ -3,5 +3,5 @@ import json, sys
 params = sys.argv
 proxmox = ProxmoxAPI(params[1], user=params[2], password=params[3], verify_ssl=False)
 result = proxmox.cluster.resources.get()
-s1 = json.dumps(result)
+s1 = json.dumps(result, indent=2)
 print(s1)
