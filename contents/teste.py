aed26099469091df48113d0367859cdafbfb987f
saulotarsobc <saulotarsobc@gmail.com>
2021-07-21 20:41:58 -0300

    Delete teste.py

--- contents/teste.py
+++
@@ -1,9 +0,0 @@
-from proxmoxer import ProxmoxAPI
-import json
-
-proxmox = ProxmoxAPI('172.33.255.2', user='root@pam',
-                     password='10l15p130A@', verify_ssl=False, service='PMG')
-
-a = proxmox.statistics.sender.get()
-c = json_formatted_str = json.dumps(a, indent=2)
-print(c)
